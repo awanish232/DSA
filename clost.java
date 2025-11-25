01088dd7ca86ad3053c240009fe87d4eb30863ce
awanish <awanish@estreet.in>
2025-11-25 12:19:10 +0530

    added new file

---
+++ clost.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class clost {
+    public int threes(int[] nums, int target){
+        Arrays.sort(nums);
+        int n= nums.length;
+        int count=0;
+        for(int i=0; i<n-2;i++){
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum= nums[i]+nums[left]+nums[right];
+                if(sum<target){
+                    count+=(right-left);
+                    left++;
+                } else{
+                    right--;
+                }
+            }
+        }
+        return count;
+
+
+    }
+    
+}
